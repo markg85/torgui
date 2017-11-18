7082959d77fda212517fcaeab9a8ff4532b08b18
Mark Gaiser <markg85@gmail.com>
2017-11-18 14:41:50 +0100

    Update to ES6 config.

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   entry: './modules.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'stage-0']
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js'
   }
